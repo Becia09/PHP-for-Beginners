e5a35ebedd6bb7c46e6cce743b151402ae38add4
Beata <becia09269@wp.pl>
2018-08-31 18:53:26 +0200

    add forLoop.php

---
+++ forLoop.php
@@ -0,0 +1,22 @@
+<!DOCTYPE HTML>
+<html lang="en">
+
+<head>																	<!-- Treść komentarza -->
+	<meta charset="UTF-8" />
+	<title>Document </title>
+	
+</head>
+
+<body>
+
+	<?php
+	
+	for ($i = 0; $i < 10; $i++)
+	{
+		echo $i . "<br>";
+	}
+	
+	?>
+	
+<body/>
+</html>
