db6e3b886f3f7fd76b97763eb65d4f9ab940cdc2
Beata <becia09269@wp.pl>
2018-09-01 13:13:35 +0200

    foreach

---
+++ foreachLoop.php
@@ -0,0 +1,24 @@
+<!DOCTYPE HTML>
+<html lang="en">
+
+<head>																	<!-- Treść komentarza -->
+	<meta charset="UTF-8" />
+	<title>Document </title>
+	
+</head>
+
+<body>
+
+	<?php
+	
+	$numbers = array(43, 546, 4, 543, 34, 87);
+	
+	foreach ($numbers as $number)
+	{
+		echo $number . "<br>";
+	}
+	
+	?>
+	
+<body/>
+</html>
